9f70dd36d79f158d407e39f7d735d0e009b1192e
BohanJook <edvinjakobsson5@gmail.com>
2021-08-01 15:08:19 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './index.jpeg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          All hail Mr.Nimbus! <br/>
+	  Learn React or die, you absolute cabbage!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
