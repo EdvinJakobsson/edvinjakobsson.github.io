6768c789a7850dc4477a3cadaf4b0233849c1b4c
BohanJook <edvinjakobsson5@gmail.com>
2021-08-12 21:18:23 +0200

    prettier formated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './index.jpeg';
-import './App.css'
+import logo from "./index.jpeg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         <p>All hail Mr.Nimbus! <br/></p>
-	 <p>Learn React or die, you absolute cabbage!</p>
+        <p>
+          All hail Mr.Nimbus! <br />
+        </p>
+        <p>Learn React or die, you absolute cabbage!</p>
       </header>
     </div>
   );
