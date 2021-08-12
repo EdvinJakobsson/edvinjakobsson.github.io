5f6b7af3bc13450674d8c114f9060fd588e4385d
BohanJook <edvinjakobsson5@gmail.com>
2021-08-12 20:59:45 +0200

    Almost the tweet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './index.jpeg';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          All hail Mr.Nimbus! <br/>
-	  Learn React or die, you absolute cabbage!
-        </a>
+         <p>All hail Mr.Nimbus! <br/></p>
+	 <p>Learn React or die, you absolute cabbage!</p>
       </header>
     </div>
   );
